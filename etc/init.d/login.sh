37ba951f5a7dbcac0b2a5d67be1631ce017abae5
MLT <60033959+Mahmoud7Osman@users.noreply.github.com>
2021-06-04 13:51:14 -0400

    Add files via upload

---
+++ etc/init.d/login.sh
@@ -0,0 +1,5 @@
+source lib/colors2
+source lib/fsh.cfg.sh
+printf "Starting ${red}Digle${white} Framework at$green `date`..\n"
+clog "Started The Digle Shell"
+
---
+++ zstart.sh
@@ -0,0 +1,114 @@
+trap test SIGINT &> /dev/null
+# PATH
+export PODGL=$PWD
+
+#
+
+# Shell Variables & Built-in Functions
+
+source $PODGL/lib/fsh.cfg.sh
+source $PODGL/etc/envir
+source $PODGL/lib/colors2
+
+if [ "$1" != '' ];then
+    chmod +x $1
+    . ./$1
+    exit
+fi
+
+
+# motd # Message Of The Day
+cat etc/motd
+
+# History
+HISTFILE=$PODGL/var/digle_history
+set -o history
+
+
+# Simple Login System
+trap exit SIGINT
+cd $PODGL/etc/login/
+$PODGL/etc/login/login
+if [ "$?" == 1 ];then
+   cerr Password Incorrect..!
+   exit 1
+fi
+
+cd - &> /dev/null
+#
+
+# RC Script
+bash etc/rc.local
+
+# INIT.D scripts
+for i in $(ls etc/init.d);do
+   etc/init.d/$i
+done
+
+# Aliases
+alias   ls="ls --color"
+alias grep="grep --color"
+
+trap test SIGINT &> /dev/null
+
+# SHELL
+history -c
+tcheck(){
+if [ -d "$1" ];then
+     cd $1
+     ./main.*
+     cd - &> /dev/null
+     return 0
+fi
+return 1
+}
+
+
+if [ -f ".update" ];then
+   printf "${white}file ($green.update$white) Found !! Running Updater...!\n"
+   sleep 0.5
+   bash run/update
+   rm .update
+fi
+
+
+
+while [ 1 ]; do
+ cin cmd
+ if [ "$cmd" == "" ];then
+   continue
+ fi
+ # PATH
+ for path in $(/bin/cat $PODGL/etc/PATH.dgl); do
+    if [ ! -f "$path$(echo $cmd | head -n1 | awk '{print $1;}')" ];then
+      tcheck $path$cmd
+      if [ "$?" == 0 ];then continue;fi
+      # PATH
+      if [ "$path" == "$(/bin/tail -1 $PODGL/etc/PATH.dgl)" ] && [ ! -f "$(/bin/tail -1 $PODGL/etc/PATH.dgl)$cmd" ];then
+	 type $(echo $cmd | head -n1 | awk '{print $1;}') &> /dev/null
+         if [ "$?" != 0 ];then
+              cerr "$red$(echo $cmd | head -n1 | awk '{print $1;}')$white Command Execution Error: Command Not Found"
+         else
+              $cmd
+              history -s $cmd
+	      break;
+         fi
+         break;
+      # PATH
+      elif [ -f "$(/bin/tail -1 $PODGL/etc/PATH.dgl)$(echo $cmd | head -n1 | awk '{print $1;}')" ];then
+	 # PATH
+	 $(/bin/tail -1 $PODGL/etc/PATH.dgl)$cmd
+         history -s $cmd
+	 break;
+      else
+      continue
+      fi
+    continue
+    fi
+    $path$cmd
+    history -s $cmd
+    break;
+ done
+done
+
+history -c
