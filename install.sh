5d9e0ff35af245a97245661126171655a065d966
Mahmoud Osman <60033959+Mahmoud7Osman@users.noreply.github.com>
2022-11-18 01:20:26 +0500

    added -embed option

--- install.sh
+++ install.sh
@@ -1,19 +1,38 @@
-if [ $(whoami) != "root" ]; then
+EMBED=''
+printf "_________________________________________________________________\n"
+sleep 0.1
+if [ "$2" != '' ]; then
+	if [ "$1" == "-embed" ]; then
+		printf "[>] Installing Cleny To $2\n\n"
+		mkdir "$2"/bin &>/dev/null
+		EMBED="$2"
+	fi
+fi
+sleep 0.1
+if [ $(whoami) != "root" ] && [ "$EMBED" == "" ]; then
 	printf "ERROR: Sudo Privileges Required To Install Cleny On Your System\n"
+	sleep 0.1
 	printf "Please Run 'sudo bash install.sh'\n"
+	sleep 0.1
+	printf "\nDid You Mean \`./install.sh -embed <Folder>\` ?\n"
 	exit 1
 fi
 
-g++ base/cleny.cpp -o bin/cleny &> /dev/null
+printf "[+] Compiling Binaries ...\n"
+sleep 0.5
+g++ cleny/cleny.cpp -o $EMBED/bin/cleny &> /dev/null
 
-printf " -> https://github.com/Mahmoud7Osman/Cleny\n"
-printf "in the README.md section, You will find everything you need\n\n"
-printf "Thank You For Installing Cleny\n"
+printf "[i] https://github.com/Mahmoud7Osman/Cleny\n"
+sleep 1
+printf "[i] in the README.md section, You will find everything you need\n\n"
+sleep 1
+printf "[<] Thank You For Installing Cleny\n"
+sleep 0.5
+printf "_________________________________________________________________\n"
+
 
 if [ ! -f "bin/cleny" ]; then
-	cp bin/x64_cleny /bin/cleny
-	chmod uog+x /bin/cleny
+	cp bin/x64_cleny $EMBED/bin/cleny
 	exit 0
 fi
-cp bin/cleny /bin/cleny
-chmod uog+x /bin/cleny
+cp bin/cleny $EMBED/bin/cleny
