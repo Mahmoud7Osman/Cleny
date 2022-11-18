69b2bb6d03cccda42698c136ad8423185d68cd12
Mahmoud Osman <60033959+Mahmoud7Osman@users.noreply.github.com>
2022-11-18 23:44:52 +0500

    Bug Fixes

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
-EMBED=''
 printf "_________________________________________________________________\n"
 sleep 0.1
 if [ "$2" != '' ]; then
 	if [ "$1" == "-embed" ]; then
-		printf "[>] Installing Cleny To $2\n\n"
-		mkdir "$2"/bin &>/dev/null
-		EMBED="$2"
+		printf "[+] Adding Cleny to $2\n"
+		sleep 1
+		cp ../Cleny "$2" -r
+		printf "[+] Cleny Added To $2 Successfully\n"
+		exit 0
 	fi
 fi
 sleep 0.1
-if [ $(whoami) != "root" ] && [ "$EMBED" == "" ]; then
+if [ $(whoami) != "root" ] && [ "" == "" ]; then
 	printf "ERROR: Sudo Privileges Required To Install Cleny On Your System\n"
 	sleep 0.1
 	printf "Please Run 'sudo bash install.sh'\n"
@@ -20,7 +21,7 @@ fi
 
 printf "[+] Compiling Binaries ...\n"
 sleep 0.5
-g++ cleny/cleny.cpp -o $EMBED/bin/cleny &> /dev/null
+g++ cleny/cleny.cpp -o /bin/cleny &> /dev/null
 
 printf "[i] https://github.com/Mahmoud7Osman/Cleny\n"
 sleep 1
@@ -31,8 +32,7 @@ sleep 0.5
 printf "_________________________________________________________________\n"
 
 
-if [ ! -f "bin/cleny" ]; then
-	cp bin/x64_cleny $EMBED/bin/cleny
+if [ ! -f "/bin/cleny" ]; then
+	cp bin/x64_cleny /bin/cleny
 	exit 0
 fi
-cp bin/cleny $EMBED/bin/cleny
